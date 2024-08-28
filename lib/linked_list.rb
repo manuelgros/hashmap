b3beed459e983a4d8b1930567c84cf03227c2fc9
manuelgros <131806742+manuelgros@users.noreply.github.com>
2024-08-28 15:08:02 -0700

    Created LinkedList class (copied from former project)

---
+++ lib/linked_list.rb
@@ -0,0 +1,91 @@
+require './lib/node'
+
+# List class containing  the full list
+class LinkedList
+
+  # attr_accessor :name
+  attr_reader :head, :tail
+
+  def initialize
+    @head = nil
+    @tail = nil
+  end
+
+  def prepend(key, value)
+    node = Node.new(key, value)
+    if @head.nil?
+      @head = node
+      @tail = node
+    else
+      node.next_node = @head
+      @head = node
+    end
+  end
+
+  def append(key, value)
+    node = Node.new(key, value)
+    if  @head.nil?
+      @tail = node
+      @head = node
+    else
+      @tail.next_node = node
+      @tail = node
+    end
+    self
+  end
+
+  # def size
+  #   count = 0
+  #   active_node = @head
+  #   until active_node == nil
+  #     count += 1
+  #     active_node = active_node.next_node
+  #   end
+  #   count
+  # end
+
+  # def at(index)
+  #   active_node = @head
+  #   (index-1).times do 
+  #     active_node = active_node.next_node
+      
+  #     return if active_node == nil
+  #   end
+  #   active_node
+  # end
+
+  # def pop
+  #   return nil if @tail.nil?
+
+  #   active_node = @head
+  #   until active_node.next_node == @tail
+  #     active_node = active_node.next_node
+  #   end
+  #   @tail = active_node
+  #   deleted_node = active_node.next_node
+  #   active_node.next_node = nil
+  #   deleted_node
+  # end
+
+  def contains?(value)
+    active_node = @head
+    until active_node == nil
+      return true if active_node.value == value
+
+      active_node = active_node.next_node
+    end
+    false
+  end
+
+  # def find(value)
+  #   active_node = @head
+  #   index = 1
+  #   until active_node == nil
+  #     return index if active_node.value == value
+
+  #     active_node = active_node.next_node
+  #     index += 1
+  #   end
+  #   nil
+  # end
+end
